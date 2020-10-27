fed4b041d9acd0c4dfac61cbcb730c1d1628d982
Robert Leu <robertleu@gmail.com>
2020-10-27 01:19:22 -0700

    Added schemas for out data

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,35 @@
+import { FaPepperHot as icon, FaLeaf as leaf } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  // visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'What is the name of the topping?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '(Vegetarian)' : ''}`,
+    }),
+  },
+};
