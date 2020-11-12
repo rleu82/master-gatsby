aed5b7dafb40422306ff09b87ff4c2094bf7ba4d
Robert Leu <robertleu@gmail.com>
2020-11-12 01:33:07 -0800

    Fixed: Hover Rotation of Nav

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -21,22 +21,26 @@ const NavStyles = styled.nav`
   }
   li {
     --rotate: -2deg;
-    transform: rotate(-2deg);
+    transform: rotate(var(--rotate));
     order: 1;
     &:nth-child(1) {
       --rotate: 1deg;
+    }
       &:nth-child(2) {
       --rotate: -2.5deg;
+      }
       &:nth-child(4) {
       --rotate: -2.5deg;
+      }
       &:hover {
-        --rotate: 3deg;
+      --rotate: 3deg;
       }
     }
   }
   a {
     font-size: 3rem;
     text-decoration: none;
+    display: block;
     &:hover {
      color: var(--red)
    }
