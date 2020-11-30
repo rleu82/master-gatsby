14fe3048752d5a5cdba3bc4308e7e7e360b0bb18
Robert Leu <robertleu@gmail.com>
2020-11-30 00:52:00 -0800

    Home page: created styling and fetched data

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import SEO from '../components/SEO';
+import ItemGrid from '../components/ItemGrid';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      <h2 className="center">
+        <span className="mark tilt">Slicemasters On</span>
+      </h2>
+      <p>Standing By, To Slice You Up</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No One Is Working Right Now.</p>
+      )}
+      {slicemasters?.length && <ItemGrid items={slicemasters} />}
+    </div>
+  );
+}
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      <h2 className="center">
+        <span className="mark tilt">Hot Slices</span>
+      </h2>
+      <p>Come On By, Buy The Slice</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothing in the Case</p>}
+      {hotSlices?.length && <ItemGrid items={hotSlices} />}
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
   return (
-    <>
-      <SEO title="Nom Nom" />
-      <p>Hey! Im the homepage</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
+    </div>
   );
 }
---
+++ gatsby/src/utils/useLatestData.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+
+const gql = String.raw;
+
+export default function useLatestData() {
+  // hot slices
+  const [hotSlices, setHotSlices] = useState();
+  // slicemasters
+  const [slicemasters, setSlicemasters] = useState();
+  // Use a side effect to fetch data from graphql endpoint
+  useEffect(function () {
+    // when component mounts, fetch data
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: gql`
+          query {
+            StoreSettings(id: "downtown") {
+              name
+              slicemaster {
+                name
+                _id
+                image {
+                  asset {
+                    url
+                    metadata {
+                      lqip
+                    }
+                  }
+                }
+              }
+              hotSlices {
+                name
+                _id
+                image {
+                  asset {
+                    url
+                    metadata {
+                      lqip
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // TODO: check for errors
+        // set data to state
+        setHotSlices(res.data.StoreSettings.hotSlices);
+        setSlicemasters(res.data.StoreSettings.slicemaster);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return {
+    hotSlices,
+    slicemasters,
+  };
+}
